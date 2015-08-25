bbe54cf0ad203e0962bb8d630d5dc9d657ed9da8
Adam Ki Jensen <adajensen@gmail.com>
2015-08-25 17:24:20 -0600

    adding basic layout of app

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,8 +1,13 @@
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
+  get '/' do
+    erb :dashboard
+  end
+
   get '/skills' do
     erb :index
   end
+
 
 end
